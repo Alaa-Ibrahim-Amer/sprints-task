8e0072e2efdc3445b07c9ab38b4f0aee5bf91f8b
Alaa <Ala2amer@gmail.com>
2022-12-11 05:41:42 +0200

    delete files

--- js/home.js
+++
@@ -1,8 +0,0 @@
-
-const addSingleProductToCart = (product) => {
-  JSON.stringify(product);
-  const products = JSON.parse(localStorage.getItem("products") || "[]");
-  products.push(product);
-  localStorage.setItem("products", JSON.stringify(products));
- 
-};
