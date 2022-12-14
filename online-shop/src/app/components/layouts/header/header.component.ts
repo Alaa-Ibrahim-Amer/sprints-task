f4619615d81bd260666a81238ef0a802f16b9703
Alaa <Ala2amer@gmail.com>
2022-12-14 04:06:52 +0200

    Implement like button logic

---
+++ online-shop/src/app/components/layouts/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  constructor(public productService:ProductService){
+  }
+
+
+}
---
+++ online-shop/src/app/services/product.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/environment';
+import { Product } from '../interfaces/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  cartProducts: Product[] = [];
+  Likes:number=0;
+
+  constructor(private httpClient: HttpClient) {}
+
+  getFeaturedProducts(): any {
+    return this.httpClient.get(`${environment.apiUrl}products/getFeatured`);
+  }
+
+  getProducts(): any {
+    return this.httpClient.get(`${environment.apiUrl}products`);
+  }
+
+  getRecentProducts(): any {
+    return this.httpClient.get(`${environment.apiUrl}products/getRecent`);
+  }
+
+  addProduct(product: Product): void {
+    this.cartProducts.push(product);
+  }
+
+  getProductById(id:string){
+    return this.httpClient.get(`${environment.apiUrl}products/${id}`)
+  }
+
+  getProductByCategoryId(id:string){
+    return this.httpClient.get(`${environment.apiUrl}products/getByCategoryId/${id}`);
+  }
+  addLike():any{
+    return this.Likes++;
+  }
+}
