877396a289452baf08c403802dd298694b86e770
Alaa <Ala2amer@gmail.com>
2023-01-11 01:11:05 +0200

    implement cart page with logic

--- online-shop/logic/cart.php
+++ online-shop/logic/cart.php
@@ -27,3 +27,48 @@ function getCart()
     $cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
     return $cart;
 }
+function getSubTotal()
+{
+    $cart = getCart();
+    $SubTotal = 0;
+    for ($i = 0; $i < count($cart); $i++) {
+        $SubTotal += $cart[$i]['product']['price'] * $cart[$i]['quantity'];  
+    }
+    return $SubTotal;
+
+}
+function getShiping()
+{
+    $cart = getCart();
+    $Shiping = 0;
+    for ($i = 0; $i < count($cart); $i++) {
+        $Shiping += 2 ;  
+    }
+    return $Shiping;
+}
+function getTotal()
+{
+    return getShiping() + getSubTotal();
+}
+
+function incQuantity($cart,$i)
+{
+    $quantity = $cart[$i]['quantity'] + 1;
+    $cart[$i]['quantity'] = $quantity;
+    $_SESSION['cart'] = $cart;
+
+}
+function decQuantity($cart,$i)
+{
+    $quantity = $cart[$i]['quantity'] - 1;
+    if($quantity > 0)
+        $cart[$i]['quantity'] = $quantity;
+    else if ($quantity <= 0)
+        unset($cart[$i]);
+    $_SESSION['cart'] = $cart;
+}
+function deleteLine($cart,$i)
+{
+    unset($cart[$i]);
+    $_SESSION['cart'] = $cart;
+}
